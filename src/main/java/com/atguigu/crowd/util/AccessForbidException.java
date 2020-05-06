20fa24ccfca15918872090306f44e42b2ad9bb74
explore11 <prefect_start@163.com>
2020-05-06 23:18:22 +0800

    增加自定义异常

---
+++ src/main/java/com/atguigu/crowd/util/AccessForbidException.java
@@ -0,0 +1,28 @@
+package com.atguigu.crowd.util;
+
+/**
+ * @program: atcrowdfunding05-common-util
+ * @description //TODO
+ * @author: swq
+ * @create: 2020-05-06 22:17
+ **/
+public class AccessForbidException extends RuntimeException{
+    public AccessForbidException() {
+    }
+
+    public AccessForbidException(String message) {
+        super(message);
+    }
+
+    public AccessForbidException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AccessForbidException(Throwable cause) {
+        super(cause);
+    }
+
+    public AccessForbidException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
