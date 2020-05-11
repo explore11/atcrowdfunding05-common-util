9764471b1d2c737537c88dd68a1368254ddb5c36
explore11 <prefect_start@163.com>
2020-05-12 00:03:14 +0800

    增加自定义常量

--- src/main/java/com/atguigu/crowd/util/CrowdConstants.java
+++ src/main/java/com/atguigu/crowd/util/CrowdConstants.java
@@ -8,5 +8,6 @@ public class CrowdConstants {
     public static final String MESSAGE_STRING_INVALIDATE = "输入的是无效的字符串";
     public static final String SYSTEM_ERROR_ACCOUNT_NOT_UNIQUE = "系统错误:账号不唯一";
     public static final String LOGIN_ADMIN = "loginAdmin";
+    public static final String ATTR_NAME_PAGE_INFO = "pageInfo";
 
 }
